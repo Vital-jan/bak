41fe88aa75254f5271752af2e8642727ec1594f5
unknown <vitaljan@gmail.com>
2019-03-06 00:24:39 +0200

    2019-03-06

--- template/news_view.php
+++ template/news_view.php
@@ -9,14 +9,14 @@
     $login = getLogin();
 
     if ($login) {
-        echo "<button type='button'> Додати новину </button><br>"; 
+        echo "<button type='button' id='news-add'> Додати новину </button><br>"; 
     };
     foreach($news as $key=>$value) {
             $value[header] = strip_tags($value[header],'<br>');
             $value[date] = strip_tags($value[date],'<br>');
             $value[content] = strip_tags($value[content],'<br>');
             $btns = '';
-            if ($login) $btns = "<img class='edit-button' src='../assets/img/edit-button.png'><img class='edit-button' src='../assets/img/close.png'>";
+            if ($login) $btns = "<img class='edit-button' id='news-edit' data-id={$value['news_id']} src='../assets/img/edit-button.png'><img class='edit-button' id='news-del' data-id={$value['news_id']} src='../assets/img/close.png'>";
         echo "
             <div class='news-item'>
             <h2 class='news-header'>
@@ -43,5 +43,92 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }, itemTimeout);
         itemTimeout +=50;
     })
+
+function addEdit(item) { // редагування та додавання запису (item == null: додавання, item!=null - редагування, де item - ідентифікатор - ключ БД)
+
+    function addEditForm(header, number = null) { // форма редагування - додавання
+            let date = new Date();
+            let cDate = `${date.getFullYear()}-${digitalForward(date.getMonth(),2)}-${digitalForward(date.getDate(),2)}`;
+			modalWindow(header, `
+			<form name='addEdit'  class="admin">
+			<ul>
+				<li><input type='date' value='${cDate}' name='date'></li>
+				<li><input type='text' placeholder='Заголовок' name='header'></li>
+                <li><textarea name='content' rows='3' placeholder='Контент'></textarea></li>
+                <li><button type='button'>Обрати зображення</button></li>
+                <li><button type='button'>Завантажити зображення</button></li>
+			</ul>
+			`
+			, ['+Зберегти', '-Скасувати'], (btn)=>{
+				let formAdmin = document.forms.addEdit;
+				if (btn == 0) { // збереження форми в базі
+					if (!item) { // додаваня запису
+					queryInsert('news', [
+						['date', formAdmin.date.value],
+						['header', formAdmin.header.value],
+						['content', formAdmin.content.value]
+					], (response)=>{
+						if (!response.sql) {console.log(response)} else {
+							alert ('Запис додано.');
+							document.location.reload(true);
+						};
+					}, '<?=PHP_PATH?>');
+				} // додаваня запису
+					if (item != null) { // редагування запису
+					queryUpdate('news', `news_id=${item}`, [
+						['date', formAdmin.date.value],
+						['header', formAdmin.header.value],
+						['content', formAdmin.content.value]
+					], (response)=>{
+						if (!response.sql) {console.log(response)} else {
+							alert ('Запис змінено.');
+							document.location.reload(true);
+						};
+					}, '<?=PHP_PATH?>');
+				} // редагування запису
+
+				} // збереження форми в базі
+			},
+			'80%', 300); // modalwindow
+
+        } // addEditForm
+        
+        if (item == null) { // додавання новини
+			addEditForm ('Додати новину');
+		}
+		else { // редагування новини
+            addEditForm('Редагувати новину', item);
+            let formAdmin = document.forms.addEdit;
+			queryGet(`select * from news where news_id=${item}`, (response)=>{ // отримуємо елемент з бази
+                // наповнюємо поля форми
+				formAdmin.date.value = response[0].date;
+				formAdmin.header.value = response[0].header;
+				formAdmin.content.value = response[0].content;
+			}, '<?=PHP_PATH?>')
+		}
+
+}
+
+	document.body.addEventListener('click', (event)=>{ // обробка кліку edit, del та add
+		if (event.target.id == 'news-add') {
+			addEdit(null);
+		}
+		if (event.target.id == 'news-edit') {
+			addEdit(event.target.dataset.id);
+			}
+		if (event.target.id == 'news-del') { // видалення новини
+			modalWindow('Видалення елементу', 'Ви підтверджуєте видалення цього елементу?', ['Залишити', '-Видалити'], (n)=>{
+				if (n == 1) {
+					queryDelete('news', `news_id=${event.target.dataset.id}`, (response)=>{
+						if (!response.sql) {console.log(response)} else {
+							alert ('Запис видалено.');
+							document.location.reload(true);
+						}
+					}, '<?=PHP_PATH?>');
+				}
+			}, '60%');
+		};
+	})
+
 }) // onload
 </script>
